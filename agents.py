ad8addb353e6ea9361c2e316d142a6a98b075dde
vyom10445 <vyom.2427030058@muj.manipal.edu>
2026-07-14 14:58:26 +0530

    news tool

--- agents.py
+++ agents.py
@@ -7,7 +7,7 @@ from langchain_mistralai import ChatMistralAI
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage , ToolMessage
 from tavily import TavilyClient
-
+from rich import print
 
 #create some tools
 
@@ -33,5 +33,51 @@ def get_weather(city: str) -> str:
 
 
 
+# news tool (tavily)
 
-print(get_weather.invoke("Kanpur"))
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def get_news(city: str) -> str:
+    """Get latest news about a city"""
+    
+    response = tavily_client.search(
+        query=f"latest news in {city} in English",
+        search_depth="basic",
+        max_results=3
+    )
+    
+    results = response.get("results", [])
+    
+    if not results:
+        return f"No news found for {city}"
+    
+    news_list = []
+    
+    for r in results:
+        title = r.get("title", "No title")
+        url = r.get("url", "")
+        snippet = r.get("content", "")
+        
+        news_list.append(
+            f"- {title}\n  🔗 {url}\n  📝 {snippet[:100]}..."
+        )
+    
+    return f"Latest news in {city}:\n\n" + "\n\n".join(news_list)
+
+
+
+
+llm = ChatMistralAI(model="mistral-small-2506")
+
+
+tools = {
+    get_weather : get_weather,
+    get_news : get_news
+}
+
+
+llm_with_tools = llm.bind([get_weather , get_news])
+
+
+#  AGENT LOOP
